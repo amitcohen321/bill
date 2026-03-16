01d599bd236de97bee3d9162379671ead4296038
Amit Cohen <amit.c@cheq.ai>
2026-03-16 19:39:20 +0200

    CORS fix

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
 
   app.enableCors({
-    origin: process.env['NODE_ENV'] === 'production' ? false : 'http://localhost:5173',
+    origin: process.env['NODE_ENV'] === 'production' ? false : true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
 
